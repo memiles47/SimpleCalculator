372abaed1ac9a63b0c733c663999b15ea6cf283b
Michael E Miles <michael@miles-me.net>
2017-09-25 03:23:55 -0500

    Add comments delinating what I am doing for future reference

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -6,12 +6,17 @@ namespace SimpleCalculator
     {
         private static void Main()
         {
+            //Create new person
             var person = new Person();
 
+            //Get age from person
             Console.WriteLine(person.GetAge());
+
+            //Assign new age
             person.SetAge(29);
             Console.WriteLine(person.GetAge());
 
+            //Access static Greeting method
             Person.Greeting();
             Console.ReadKey();
         }
